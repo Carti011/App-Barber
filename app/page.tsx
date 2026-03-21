69328e0e234546a6263241aedf09b7e5a91c0e4c
Weslley Cardoso <weslleycardoso011@gmail.com>
2026-03-21 14:33:53 -0300

    feat(home): adicionar header e texto de boas-vindas

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,18 @@
+import Header from "./_components/header";
+
 const Home = () => {
-  return null;
+  return (
+    <div>
+      {/* Header */}
+      <Header />
+
+      {/* Texto */}
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Felipe! </h2>
+        <p>Segunda-feira, 05 de agosto.</p>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
